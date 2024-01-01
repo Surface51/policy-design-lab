0dce8a405962c9a4b5198a751914d2a2478f0baa
John Runyard <jrunyard3ft@gmail.com>
2024-01-01 14:50:09 -0600

    Updated copy.

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,16 @@ function App() {
           <h3>Map Visualization</h3>
         </div>
         <div className="content-container">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus
-          eget nunc scelerisque viverra mauris. Bibendum arcu vitae elementum
-          curabitur vitae nunc sed velit dignissim. Fames ac turpis egestas
-          maecenas pharetra convallis posuere morbi. Ipsum a arcu cursus vitae
-          congue mauris rhoncus aenean. Eu facilisis sed odio morbi quis commodo
-          odio. Diam volutpat commodo sed egestas egestas fringilla phasellus
-          faucibus scelerisque. Donec ultrices tincidunt arcu non sodales neque
-          sodales. Etiam sit amet nisl purus in mollis nunc sed. Imperdiet dui
-          accumsan sit amet nulla facilisi. Scelerisque viverra mauris in
-          aliquam. Iaculis at erat pellentesque adipiscing. Enim tortor at
-          auctor urna nunc.
+          <b>The value of visualization.</b> The data visualizations below
+          provide a proof of concept showcasing how
+          <b>modeling and research can be applied to policy innovations.</b>
+          Specifically, these visuals offer insight into changes to a particular
+          farm payment program—the Agriculture Risk Coverage, county coverage
+          option (ARC-CO). By choosing a year, state, and crop, you can swiftly
+          see the results of changes to ARC-CO. The visualization will then
+          provide a comparison of how these changes in policy affect payments to
+          farmers. The results offer a clearer understanding of how policy
+          changes can impact farm economics, conservation, and more.
         </div>
       </section>
       <Map />
@@ -29,18 +27,10 @@ function App() {
           <h3 className="color-primary">Why is this important?</h3>
         </div>
         <div className="content-container">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus
-          eget nunc scelerisque viverra mauris. Bibendum arcu vitae elementum
-          curabitur vitae nunc sed velit dignissim. Fames ac turpis egestas
-          maecenas pharetra convallis posuere morbi. Ipsum a arcu cursus vitae
-          congue mauris rhoncus aenean. Eu facilisis sed odio morbi quis commodo
-          odio. Diam volutpat commodo sed egestas egestas fringilla phasellus
-          faucibus scelerisque. Donec ultrices tincidunt arcu non sodales neque
-          sodales. Etiam sit amet nisl purus in mollis nunc sed. Imperdiet dui
-          accumsan sit amet nulla facilisi. Scelerisque viverra mauris in
-          aliquam. Iaculis at erat pellentesque adipiscing. Enim tortor at
-          auctor urna nunc.
+          Data visualization can help you make better, more informed policy
+          decisions. You can more easily see important information, analyze
+          granular concrete data, and implement policies according to what works
+          best for a specific area.
         </div>
       </section>
     </main>
