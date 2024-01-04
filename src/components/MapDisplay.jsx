41ab9fdc1e89a27621fa5bd9742e07d5c631c42c
John Runyard <jrunyard3ft@gmail.com>
2024-01-04 17:09:24 -0600

    Added zoome update and annotations.

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -3,10 +3,15 @@ import {
   ComposableMap,
   Geographies,
   Geography,
+  Marker,
+  Annotation,
   ZoomableGroup,
 } from "react-simple-maps";
 import { scaleQuantize } from "d3-scale";
 import { csv, json } from "d3-fetch";
+import { geoCentroid } from "d3-geo";
+
+import allStates from "../data/allstates.json";
 
 const colorScale = scaleQuantize()
   .domain([0, 100])
@@ -23,6 +28,18 @@ const colorScale = scaleQuantize()
     "#184E77",
   ]);
 
+const offsets = {
+  VT: [50, -8],
+  NH: [34, 2],
+  MA: [30, -1],
+  RI: [28, 2],
+  CT: [35, 10],
+  NJ: [34, 1],
+  DE: [33, 0],
+  MD: [47, 10],
+  DC: [49, 21],
+};
+
 const MapDisplay = ({ year, crop, state, cropTypes, setCropTypes }) => {
   const [data, setData] = useState([]);
   const [stateGeo, setStateGeo] = useState([]);
@@ -30,6 +47,9 @@ const MapDisplay = ({ year, crop, state, cropTypes, setCropTypes }) => {
   const [loaded, setLoaded] = useState(false);
   const benchmark_ratio = 0.86;
   const [yearData, setYearData] = useState({});
+  const [center, setCenter] = useState([-96, 37.8]);
+  const [zoomLevel, setZoomLevel] = useState(1);
+  const [stateGeoData, setStateGeoData] = useState([]);
 
   useEffect(() => {
     json("json/counties-10m.json").then((data) => {
@@ -83,10 +103,38 @@ const MapDisplay = ({ year, crop, state, cropTypes, setCropTypes }) => {
     }
   }, [crop, loaded]);
 
+  useEffect(() => {
+    if (!stateGeoData || !state) {
+      return;
+    }
+
+    if (state === "all") {
+      setCenter([-96, 37.8]);
+      setZoomLevel(1);
+      return;
+    }
+
+    const geoID = allStates.find((s) => s.id === state);
+    const geo = stateGeoData.find((s) => s.id === geoID.val);
+
+    if (geo) {
+      const centroid = geoCentroid(geo);
+      setCenter(centroid);
+      setZoomLevel(3);
+    }
+  }, [stateGeoData, state]);
+
   return (
     <>
       <ComposableMap projection="geoAlbersUsa" className="map-display">
-        <ZoomableGroup>
+        <ZoomableGroup
+          center={center}
+          zoom={zoomLevel}
+          onMoveEnd={(zoom) => {
+            setCenter(zoom.coordinates);
+            setZoomLevel(zoom.zoom);
+          }}
+        >
           <Geographies geography={geo} borders={"#fff"}>
             {({ geographies }) => {
               return geographies.map((g) => {
@@ -151,36 +199,46 @@ const MapDisplay = ({ year, crop, state, cropTypes, setCropTypes }) => {
           </Geographies>
           <Geographies
             geography={stateGeo}
-            style={{ position: "absolute", top: 0 }}
+            style={zoomLevel < 2 ? { display: "block" } : { display: "none" }}
           >
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  className="state"
-                  style={{
-                    default: {
-                      fill: "transparent",
-                      stroke: "#000",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                    },
-                    hover: {
-                      fill: "#000",
-                      fillOpacity: 0.2,
-                      stroke: "#000",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                    },
-                    pressed: {
-                      fill: "#000",
-                      outline: "none",
-                    },
-                  }}
-                />
-              ))
-            }
+            {({ geographies }) => {
+              setStateGeoData(geographies);
+              return (
+                <>
+                  {geographies.map((geo) => (
+                    <Geography
+                      key={geo.rsmKey}
+                      geography={geo}
+                      className="state"
+                      style={{
+                        default: {
+                          fill: "transparent",
+                          stroke: "#000",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                        hover: {
+                          fill: "#000",
+                          fillOpacity: 0.2,
+                          stroke: "#000",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                        pressed: {
+                          fill: "#000",
+                          outline: "none",
+                        },
+                      }}
+                    />
+                  ))}
+                  {geographies.map((geo) => {
+                    const centroid = geoCentroid(geo);
+                    const cur = allStates.find((s) => s.val === geo.id);
+                    return <g key={geo.rsmKey + "-name"}></g>;
+                  })}
+                </>
+              );
+            }}
           </Geographies>
         </ZoomableGroup>
       </ComposableMap>
